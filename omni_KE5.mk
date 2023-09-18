13c206326e8a1932d791abb0273768bc8b69ee1b
Kamaldeen Jimoh <jimohkamal446@gmail.com>
2023-09-18 14:39:50 +0100

    changes

--- omni_KE5.mk
+++ omni_KE5.mk
@@ -7,7 +7,7 @@
 
 # Inherit from those products. Most specific first.
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/omni_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common omni stuff.
 $(call inherit-product, vendor/pb/config/common.mk)
